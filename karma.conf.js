4f15ea2b7b85d01f6d19cfb00b3421f09e6707bf
javarome <javarome@gmail.com>
2014-12-14 13:26:27 +0100

    Added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -30,15 +30,18 @@ module.exports = function (config) {
             'node_modules/**/tests/*.*'
         ],
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        reporters: ['progress', 'coverage'],
+
         preprocessors: {
+            'out/**/*.js': ['coverage']
         },
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        coverageReporter: {
+            reporters: [
+                {type: 'html', subdir: 'html'},
+                {type: 'lcovonly', subdir: 'lcov'}
+            ]
+        },
 
         // web server port
         port: 9876,

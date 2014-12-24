b1133210cae509cddf9b19ad4604ba34cfbaa067
javarome <javarome@gmail.com>
2014-12-24 02:18:19 +0100

    Fixed support for h2 elements instead of section's title

--- js/nav/nav-service.js
+++ js/nav/nav-service.js
@@ -283,13 +283,17 @@ angular.module('rr0.nav')
                 addSection: function (s, elem) {
                     var l;
                     var outlineL;
-                    if (s.indexOf('<h') < 0) {
+                    var hPos = s.indexOf('<h');
+                    if (hPos < 0) {
                         l = '<h1>' + s + '</h1>';
                         var tag = 'h' + (this.currentLevel) + '>';
                         outlineL = '<' + tag + s + '</' + tag;
                     } else {
                         l = s;
                         outlineL = l;
+                        var hPosEnd = s.indexOf('>', hPos+1);
+                        var h2Pos = s.indexOf('</h', hPosEnd+1);
+                        s = s.substring(hPosEnd+1, h2Pos);
                     }
                     this.currentLevel++;
                     var levelSections = sections[this.currentLevel];
@@ -298,10 +302,12 @@ angular.module('rr0.nav')
                     }
                     levelSections.push(l);
 
+                    var idLink = commonsService.validLink(s);
+                    var sectionId = commonsService.camelize(idLink, this.currentLevel);
                     var section = {
                         label: l,
                         outlineLabel: outlineL,
-                        id: commonsService.camelize(commonsService.validLink(s), this.currentLevel),
+                        id: sectionId,
                         level: this.currentLevel,
                         elem: elem
                     };

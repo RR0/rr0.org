9024386fdd6c6f782bc640ebea9e4699df04a951
javarome <javarome@gmail.com>
2015-01-17 14:30:15 +0100

    Common tests

---
+++ test/CommonSpec.js
@@ -0,0 +1,21 @@
+describe("Common module", function () {
+    'use strict';
+
+    var commonsService;
+    var $rootScope;
+    var $provide;
+
+    beforeEach(angular.mock.module('rr0.commons', function (_$provide_) {
+        $provide = _$provide_;
+    }));
+
+    beforeEach(angular.mock.inject(function (_$rootScope_, _commonsService_) {
+        commonsService = _commonsService_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('convert strings into valid links', function () {
+        expect(commonsService.validLink('Bjørn')).toEqual("Bjorn");
+        expect(commonsService.validLink('Jérôme Beau')).toEqual("JeromeBeau");
+    });
+});

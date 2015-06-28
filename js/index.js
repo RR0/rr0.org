ee736e719ed841445a0e03bde521469a21fa3c54
javarome <javarome@gmail.com>
2015-06-29 00:11:46 +0200

    Disable debug log in production

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-angular.module('rr0', ['rr0.nav', 'rr0.place', 'rr0.foot', 'rr0.context']);
+angular.module('rr0', ['rr0.nav', 'rr0.place', 'rr0.foot', 'rr0.context'])
+.config(['$logProvider', function($logProvider) {
+    'use strict';
+    $logProvider.debugEnabled(false);
+  }]);

3171e62b86dfed01d48419b86f31c9a5a9c710dd
javarome <javarome@gmail.com>
2015-12-06 23:28:55 +0100

    File sort for angular sources

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var injectFiles = require('gulp-inject');
+var angularFilesort = require('gulp-angular-filesort');
 
 var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
@@ -20,39 +21,45 @@ var usemin = require('gulp-usemin');
 
 gulp.task('clean', function () {
   'use strict';
-  return del(['dist/**/*']);
+  return del.sync(['dist/**/*']);
 });
 
-gulp.task('compile', ['clean'], function () {
-  'use strict';
-  return gulp.src([
-      '**/*.scss',
-      '!node_modules/**/*', '!bower_components/**/*', '!test/**/*'
-    ])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
-});
-
-var sources = gulp.src([
-  '**/*.js', 'dist/css/**/*',
-  '!node_modules/**/*', '!bower_components/**/*',
+var exclusions = ['!node_modules/**/*', '!bower_components/**/*',
   '!test/**/*', '!coverage/**/*', '!karma*.*',
   '!gulpfile*.*', '!gruntfile*.*',
-  '!server.js', '!rr0.js', '!dist/*.js'
-], {read: false});
+  '!server.js', '!rr0.js', '!dist/*.js'];
+
+var jsSources = ['**/*.js'].concat(exclusions);
+
+gulp.task('sass', function () {
+  'use strict';
+  return gulp.src(['**/*.scss'].concat(exclusions))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', function () {
+  'use strict';
+  return gulp.src(jsSources)
+    .pipe(es6())
+    .pipe(ngAnnotate())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile', ['clean', 'js', 'sass'], function () {
+});
 
 gulp.task('dist', ['compile'], function () {
   'use strict';
 
   return gulp.src('index2.html')
-    .pipe(injectFiles(sources))
+    .pipe(injectFiles(gulp.src(['dist/**/*.js']).pipe(angularFilesort())))
+    .pipe(injectFiles(gulp.src(['dist/**/*.css'])))
     .pipe(usemin({
       css: [minifyCss(), rev()],
       html: [minifyHtml({empty: true})],
       js: [
         sourcemaps.init(),
-        es6(),
-        ngAnnotate(),
         uglify().on('error', gulpUtil.log),
         rev(),
         sourcemaps.write('.')

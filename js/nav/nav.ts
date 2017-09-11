9f41fe652fd9b07b5466e06f2c783484a733ade1
javarome <javarome@gmail.com>
2017-09-11 09:59:32 +0200

    Moved route handling into nav module

--- js/nav/nav.ts
+++ js/nav/nav.ts
@@ -732,14 +732,114 @@ class HeadController {
   }
 }
 
+function handleUrlRedirect() {
+  'use strict';
+  return null;
+  /*  var path = location.pathname;
+   if (path.indexOf('index.new') < 0) {
+   location.href = '/index.new.html?p=' + path;
+   path = null;
+   } else {
+   path = location.search.substring(location.search.indexOf('?p=') + 3);
+   }
+   return path;*/
+}
+
+function pageContents(template) {
+  'use strict';
+  const startMark = '<div class="text">';
+  const startIndex = template.indexOf(startMark);
+  const endMark = '</div><!--text-->';
+  const endIndex = template.indexOf(endMark);
+  const templateContents = template.substring(startIndex + startMark.length, endIndex);
+  return templateContents;
+}
+
 export default navModule => {
-  var navModule = angular.module('rr0.nav', ['ngSanitize', /*'sun.scrollable', */ 'rr0.lang', 'rr0.people', 'rr0.time']);
+  var navModule = angular.module('rr0.nav', ['ngSanitize', 'ui.router', 'rr0.lang', 'rr0.people', 'rr0.time']);
   lang(navModule);
   people(navModule);
   time(navModule);
   return navModule
     .value('host', location.host)
     .constant('hiddenPos', '-100em')
+    .config(function (/*$urlRouterProvider, */$stateProvider) {
+      'use strict';
+      $stateProvider
+        .state('any', {
+          url: '*path',
+          views: {
+            'textView': {
+              templateProvider: function ($stateParams, $http) {
+                const url = location.pathname;
+                return $http.get(url).then(function (response) {
+                  const template = response.data;
+                  return pageContents(template);
+                });
+              },
+              controller: function () {
+                console.log('time controller');
+              }
+            }
+          }
+        })
+        .state('page', {
+          url: '/index.new.html?p =:path',
+          templateUrl: function ($stateParams) {
+            return $stateParams.path;
+          },
+          controller: function ($scope) {
+            console.log('ok');
+          }
+        })
+        .state('home', {
+          url: '^/dist/home',
+          views: {
+            'textView': {
+              templateUrl: './home.new.html',
+              controller: function ($scope) {
+                console.log('home controller');
+              }
+            }
+          }
+        })
+        .state('time', {
+          url: '^/dist/time',
+          views: {
+            'textView': {
+              templateProvider: function ($stateParams, $http) {
+                return $http.get('/time').then(function (response) {
+                  const template = response.data;
+                  return pageContents(template);
+                });
+              },
+              controller: function () {
+                console.log('time controller');
+              }
+            }
+          }
+        });
+      /*  $urlRouterProvider.otherwise(function($injector, $location) {
+       var path = $location.path(), normalized = path.toLowerCase();
+       if (path !== normalized) {
+       //instead of returning a new url string, I'll just change the $location.path directly so I don't have to worry about constructing a new url string and so a new state change is not triggered
+       $location.replace().path(normalized);
+       }
+       });*/
+    }).run(function ($state, $rootScope) {
+      'use strict';
+      $state.go('home')
+        .then(val => {
+          console.log('going home: ', val)
+        })
+        .catch(err => {
+          console.log('error when going home: ', err)
+        });
+      /*    var page = handleUrl();
+       if (page) {
+       $state.go('page', {path:page}, {location:false});
+       }*/
+    })
     .filter('unsafe', ['$sce', function ($sce) {
       'use strict';
       return function (val) {

135bcfe06d5eca6bd38462f49aa6038338bfe24b
Jérôme Beau <javarome@gmail.com>
2022-10-29 22:58:32 +0200

    Renmed case index.json to case.json

--- tool/step/CaseDirectoryStep.ts
+++ tool/step/CaseDirectoryStep.ts
@@ -49,10 +49,10 @@ export class CaseDirectoryStep extends DirectoryStep {
       const dirCase: Case = {dirName, time: "", title: ""}
       cases.push(dirCase)
       try {
-        const jsonFileInfo = getFileInfo(context, `${dirName}/index.json`)
+        const jsonFileInfo = getFileInfo(context, `${dirName}/case.json`)
         Object.assign(dirCase, JSON.parse(jsonFileInfo.contents))
       } catch (e) {
-        console.warn(`${dirName} has no index.json description`)
+        console.warn(`${dirName} has no case.json description`)
         // No json, just guess title.
       }
     }

1f84d1ef723f5bae53f0cedf2a37567e21206ca3
Jérôme Beau <javarome@gmail.com>
2022-11-02 11:56:38 +0100

    places lookup

--- tool/step/content/replace/html/place/PlaceReplacer.test.ts
+++ tool/step/content/replace/html/place/PlaceReplacer.test.ts
@@ -7,9 +7,25 @@ import {OrganizationService} from "../../../../../model/org/OrganizationService"
 describe("PlaceReplacer", () => {
 
   test("replace a place tag", async () => {
-    const service = new PlaceService()
-    await service.read("place/amer/n/OakRidge")
-    const replacer = new PlaceReplacer(service)
+    const location = {lat: 35.8440582, lng: -106.287162}
+    const elevation = {elevation: 2161.025390625}
+    const placeService = new class extends PlaceService {
+      async read(fileName: string): Promise<Place> {
+        return new Place(location, elevation, "")
+      }
+
+      protected async geocode(address: string): Promise<{ location: PlaceLocation; data: any } | undefined> {
+        return {location, data: {}}
+      }
+
+      protected async elevation(location: PlaceLocation): Promise<Elevation> {
+        return elevation
+      }
+    }("place")
+    const orgService = new class extends OrganizationService {
+    }("org")
+    await placeService.read("place/loc35.8440582_-106.287162.json")
+    const replacer = new PlaceReplacer(placeService, orgService)
     const context = testUtil.newHtmlContext("place/amer/n/OakRidge/index.html", "")
     const doc = context.inputFile.dom.window.document
     const placeTag = doc.createElement("span")

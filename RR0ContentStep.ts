594bce7557f31d1ea1cc68d35e1d27d4abb7c6ef
Jérôme Beau <javarome@gmail.com>
2022-11-20 17:14:01 +0100

    RR0 content step

---
+++ RR0ContentStep.ts
@@ -0,0 +1,11 @@
+import {ContentStep, ContentStepConfig} from "ssg-api"
+import {RR0SsgContext} from "./RR0SsgContext"
+
+export class RR0ContentStep extends ContentStep {
+
+  protected processFile(context: RR0SsgContext, filePath: string, contentsConfig: ContentStepConfig,
+                        contentCount: number): Promise<number> {
+    context.time.reset()  // Don't use time context from previous page.
+    return super.processFile(context, filePath, contentsConfig, contentCount)
+  }
+}

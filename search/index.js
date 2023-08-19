37d4ae9bb6ff506d211249575233ff24dd07862d
jerome <javarome@gmail.com>
2023-08-19 11:18:17 +0200

    Adamski

--- search/index.js
+++ search/index.js
@@ -34,4 +34,6 @@ function siteSearchLoad() {
 const search = document.querySelector("#main-nav .search")
 search.onmouseover = siteSearchLoad
 const searchInput = document.querySelector("#search-site")
+searchInput.disabled = false
+searchInput.setAttribute("placeHolder", "Recherche")
 searchInput.oninput = (event) => siteSearchChange(event)

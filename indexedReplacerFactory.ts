17767dcc54f7138757364650cad9d7245bee203d
jerome <javarome@gmail.com>
2023-10-29 14:15:07 +0100

    docs

--- indexedReplacerFactory.ts
+++ indexedReplacerFactory.ts
@@ -8,13 +8,11 @@ class IndexedReplacer implements DomReplacement<HtmlRR0SsgContext> {
     const items = element.querySelectorAll('li');
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
-      const content = item.firstChild;
-      if (content.nodeType === 3) {
-        const link = context.outputFile.document.createElement('a');
-        link.href = '#' + i;
-        link.appendChild(content.cloneNode(true));
-        item.replaceChild(link, content);
-        item.id = String(i);
+      const content = item.innerHTML;
+      if (!content.startsWith('<a href="#')) {
+        const n = i + 1;
+        item.id = String(n);
+        item.innerHTML = `<a href="#${n}">${content}</a>`;
         context.logger.log(item.outerHTML);
       }
     }

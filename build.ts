3b6fc712ebff711e9ab252fdee3f6876e0ded8f1
jerome <javarome@gmail.com>
2023-10-29 23:07:53 +0100

    docs

--- build.ts
+++ build.ts
@@ -80,6 +80,7 @@ const copies = copiesArg ? copiesArg.split(',') : [
   '**/*.cmmn', '**/*.bpmn',
   'people/index.js', 'people/lier.svg', 'people/index.css',
   'search/index.js', 'search/index.json',
+  'index/index.js',
   'udb/**/*.js',
   'udb/netlify.toml',
   'udb/input/db/udb/data/*.*'

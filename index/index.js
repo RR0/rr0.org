0b8fd835642f2fb138258f9f47838008974799ae
javarome <javarome@gmail.com>
2023-11-11 12:51:49 +0100

    docs

--- index/index.js
+++ index/index.js
@@ -10,6 +10,11 @@ export class Filterform {
   #langs
 
   /**
+   * @member {string}
+   */
+  #formSelector
+
+  /**
    *
    * @param {string} formSelector
    * @param {function} tagSelector
@@ -17,14 +22,14 @@ export class Filterform {
    * @param {object} langs Key-values of languages
    * @param {function} tagAttribute returns the attribute that will provide a possible value to check.
    */
-  constructor(formSelector, tagSelector, checked, langs, tagAttribute) {
-    this.formSelector = formSelector
+  constructor (formSelector, tagSelector, checked, langs, tagAttribute) {
+    this.#formSelector = formSelector
     this.#tagSelector = tagSelector
     this.checked = checked
     this.#langs = langs
     this.tagAttribute = tagAttribute
-    if (document.readyState === "loading") {
-      document.addEventListener("DOMContentLoaded", ready)
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', ready)
     } else {
       this.ready()
     }
@@ -35,10 +40,10 @@ export class Filterform {
    * @param {string} value
    * @return {HTMLElement[]}
    */
-  getTags(value) {
+  getTags (value) {
     const selector = this.#tagSelector(value)
     const tags = Array.from(document.querySelectorAll(selector))
-      .filter(element => !["HTML", "I"].includes(element.nodeName))
+      .filter(element => !['HTML', 'I'].includes(element.nodeName))
     const lang = this.#langs[value]
     return lang ? tags.map(tag => {
       const transform = lang.transform
@@ -46,39 +51,57 @@ export class Filterform {
     }) : tags
   }
 
-  clicked() {
-    const inputs = document.querySelectorAll(`form input[type="checkbox"]`)
+  clicked () {
+    const inputs = Array.from(document.querySelectorAll(this.#formSelector + ` input[type="checkbox"]`))
     for (const input of inputs) {
       const tags = this.getTags(input.value)
       for (let i = 0; i < tags.length; i++) {
         const item = tags[i]
-        item.style.display = input.checked ? item.nodeName === "LI" ? "list-item" : "block" : "none"
-        const id = item.getAttribute("id")
+        item.classList.toggle('visible', input.checked)
+        item.classList.toggle('hidden', !input.checked)
+        this.hint(input, input.checked ? 'Cliquez pour masquer' : 'Cliquez pour afficher')
+        const id = item.getAttribute('id')
         if (id) {
-          item.setAttribute("value", id)
+          item.setAttribute('value', id)
         }
       }
     }
+    const checkedOnes = inputs.filter(input => input.checked)
+    if (checkedOnes.length <= 1) {
+      checkedOnes.forEach(checkedOne => {
+        checkedOne.disabled = true
+        this.hint(checkedOne, inputs.length > 1 ? 'Sélectionnez un autre choix avant de déselectionner celui-ci' : 'Seul choix disponible')
+      })
+    } else {
+      checkedOnes.forEach(checkedOne => {
+        checkedOne.disabled = false
+      })
+    }
   }
 
-  ready() {
+  hint (input, hint) {
+    const labeled = input.parentElement.nodeName === 'LABEL' ? input.parentElement : input
+    labeled.title = hint
+  }
+
+  ready () {
     const ps = this.getTags()
     const values = ps.reduce((ls, p) => {
       ls.add(this.tagAttribute(p))
       return ls
     }, new Set())
-    const form = document.querySelector(this.formSelector)
+    const form = document.querySelector(this.#formSelector)
     for (const value of values) {
-      const label = document.createElement("label")
-      const input = document.createElement("input")
-      input.type = "checkbox"
+      const label = document.createElement('label')
+      const input = document.createElement('input')
+      input.type = 'checkbox'
       input.value = value
       input.onclick = this.clicked.bind(this)
       if (this.checked.includes(value)) {
         input.checked = true
       }
       label.appendChild(input)
-      label.append(" " + this.#langs[value].label)
+      label.append(' ' + this.#langs[value].label)
       form.append(label)
     }
     this.clicked()
--- index/lang.js
+++ index/lang.js
@@ -1,84 +1,90 @@
-import {Filterform} from "./index.js"
+import { Filterform } from './index.js'
 
-const speechLabel = "🔈"
-let speechMsg = undefined
-const transformed = "transformed"
+const speechLabel = '🔈'
+let speechMsg
+const transformed = 'transformed'
 
 const addSpeech = (e, lang = e.lang, text = e.textContent) => {
-    if (!e.classList.contains(transformed)) {
-        e.classList.add(transformed)
-        const speechEl = document.createElement("button")
-        speechEl.className = "speech"
-        speechEl.textContent = speechLabel
-        const voices = speechSynthesis.getVoices().filter(voice => voice.localService === true)
-        speechEl.title = "Écouter en " + langs[e.lang].label
-        speechEl.onclick = () => {
-            const anotherSpeech = speechMsg && speechMsg.text !== text
-            if (!speechMsg || anotherSpeech) {
-                if (anotherSpeech) {
-                    speechSynthesis.cancel()
-                }
-                speechMsg = new SpeechSynthesisUtterance()
-                const variant = navigator.language.substring(navigator.language.indexOf('-'))
-                speechMsg.voice = voices.find(voice => voice.lang === lang + variant) || voices.find(voice => voice.lang.startsWith(lang))
-                speechMsg.lang = lang
-                speechMsg.text = text
-                speechMsg.onend = () => {
-                    speechEl.textContent = speechLabel
-                }
-                speechMsg.onerror = (e) => {
-                    console.error(e)
-                    speechEl.textContent = speechLabel
-                }
-            }
-            if (speechSynthesis.paused) {
-                speechSynthesis.resume()
-                speechEl.title = "Mettre en pause"
-                speechEl.textContent = "🔇"
-            } else if (speechSynthesis.speaking) {
-                speechSynthesis.pause()
-                speechEl.title = "Reprendre"
-                speechEl.textContent = speechLabel
-            } else {
-                speechSynthesis.speak(speechMsg)
-                speechEl.title = "Mettre en pause"
-                speechEl.textContent = "🔇"
-            }
+  if (!e.classList.contains(transformed)) {
+    e.classList.add(transformed)
+    const speechEl = document.createElement('button')
+    speechEl.className = 'speech'
+    speechEl.textContent = speechLabel
+    const voices = speechSynthesis.getVoices().filter(voice => voice.localService === true)
+    speechEl.title = 'Écouter en ' + langs[e.lang].label
+    speechEl.onclick = () => {
+      const anotherSpeech = speechMsg && speechMsg.text !== text
+      if (!speechMsg || anotherSpeech) {
+        if (anotherSpeech) {
+          speechSynthesis.cancel()
         }
-        e.append(speechEl)
-        e.classList.add(transformed)
+        speechMsg = new SpeechSynthesisUtterance()
+        const variant = navigator.language.substring(navigator.language.indexOf('-'))
+        speechMsg.voice = voices.find(voice => voice.lang === lang + variant) || voices.find(voice => voice.lang.startsWith(lang))
+        speechMsg.lang = lang
+        speechMsg.text = text
+        speechMsg.onend = () => {
+          speechEl.textContent = speechLabel
+        }
+        speechMsg.onerror = (e) => {
+          console.error(e)
+          speechEl.textContent = speechLabel
+        }
+      }
+      if (speechSynthesis.paused) {
+        speechSynthesis.resume()
+        speechEl.title = 'Mettre en pause'
+        speechEl.textContent = '🔇'
+      } else if (speechSynthesis.speaking) {
+        speechSynthesis.pause()
+        speechEl.title = 'Reprendre'
+        speechEl.textContent = speechLabel
+      } else {
+        speechSynthesis.speak(speechMsg)
+        speechEl.title = 'Mettre en pause'
+        speechEl.textContent = '🔇'
+      }
     }
-    return e
+    e.append(speechEl)
+    e.classList.add(transformed)
+  }
+  return e
 }
 
 const langs = {
-    "fr": {
-        label: "Français", transform: addSpeech
-    }, "fro": {
-        label: "Français ancien", transform: addSpeech
-    }, "ar": {
-        label: "Arabe", transform: addSpeech
-    }, "ar-Latn": {
-        label: "Arabe phonétique", transform: (e) => {
-            if (!e.classList.contains(transformed)) {
-                e.innerHTML = e.innerHTML.replaceAll(/(dh|sh|gh|kh|th|hţ)/gi, `<u>$1</u>`)
-                const arEl = e.parentElement.querySelector("[lang='ar']")
-                const text = arEl.textContent.substring(0, arEl.textContent.indexOf(speechLabel))
-                addSpeech(e, "ar", text)
-            }
-            return e
-        }
-    }, "he": {
-        label: "Hébreu", transform: addSpeech
-    }, "he-Latn": {
-        label: "Hébreu phonétique", transform: (e) => {
-            const heEl = e.parentElement.querySelector("[lang='he']")
-            const text = heEl.textContent.substring(0, heEl.textContent.indexOf(speechLabel))
-            addSpeech(e, "he", text)
-        }
-    }, "img": {
-        label: "Image"
+  'fr': {
+    label: 'Français', transform: addSpeech
+  }, 'fro': {
+    label: 'Français ancien', transform: addSpeech
+  }, 'ar': {
+    label: 'Arabe', transform: (e) => {
+      e.dir = e.dir || 'rtl'
+      addSpeech(e)
+      return e
     }
+  }, 'ar-Latn': {
+    label: 'Arabe phonétique', transform: (e) => {
+      if (!e.classList.contains(transformed)) {
+        e.innerHTML = e.innerHTML.replaceAll(/(dh|sh|gh|kh|th|hţ)/gi, `<u>$1</u>`)
+        const arEl = e.parentElement.querySelector('[lang=\'ar\']')
+        if (arEl) {
+          const text = arEl.textContent.substring(0, arEl.textContent.indexOf(speechLabel))
+          addSpeech(e, 'ar', text)
+        }
+      }
+      return e
+    }
+  }, 'he': {
+    label: 'Hébreu', transform: addSpeech
+  }, 'he-Latn': {
+    label: 'Hébreu phonétique', transform: (e) => {
+      const heEl = e.parentElement.querySelector('[lang=\'he\']')
+      const text = heEl.textContent.substring(0, heEl.textContent.indexOf(speechLabel))
+      addSpeech(e, 'he', text)
+    }
+  }, 'img': {
+    label: 'Image'
+  }
 }
 
-new Filterform("#lang-form", value => value ? `*[lang="${value}"]` : "*[lang]", ["fr"], langs, p => p.lang)
+new Filterform('#lang-form', value => value ? `*[lang="${value}"]` : '*[lang]', ['fr'], langs, p => p.lang)

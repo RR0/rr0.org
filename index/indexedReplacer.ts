fac825aedabb7e8b6a7bc5f95ddd06a8b921cf50
javarome <javarome@gmail.com>
2024-01-13 15:31:15 +0100

    docs

--- index/indexedReplacer.ts
+++ index/indexedReplacer.ts
@@ -7,14 +7,14 @@ export class IndexedReplacer implements DomReplacement<HtmlRR0SsgContext> {
     const items = element.querySelectorAll('li');
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
-      const n = i + 1;
-      const id = item.getAttribute('id') || item.getAttribute('value') || i + 1;
-      const anchor = item.querySelector('.anchor');
+      const anchor = item.querySelector(':scope > .anchor');
       if (!anchor) {
         const anchorEl = context.outputFile.document.createElement('span');
+        const id = item.getAttribute('id') || item.getAttribute('value') || i + 1;
         anchorEl.id = String(id);
         anchorEl.className = 'anchor';
         item.prepend(anchorEl);
+        const n = i + 1;
         item.setAttribute('onclick', 'window.location.hash=' + n);
       }
     }

cc406948585593ea456648133bdbbe2138066398
javarome <javarome@gmail.com>
2024-01-23 21:57:46 +0100

    docs

--- people/witness/WitnessReplacer.ts
+++ people/witness/WitnessReplacer.ts
@@ -1,8 +1,9 @@
-import {SsgContext} from "ssg-api"
+import { SsgContext } from "ssg-api"
 
 export class WitnessReplacer {
 
   replacement(context: SsgContext, match: string, witnessName: string, witnessNumber: string): string {
-    return `<span class="witness" title="Nom du témoin anonymisé" style="width:${witnessName.length}em"><a href="/FAQ.html#privacy">témoin${witnessNumber ? " n° " + witnessNumber : ""}</a></span>`
+    const witnessLength = Math.max(6.5, witnessName.length)
+    return `<span class="witness" title="Nom du témoin anonymisé" style="width:${witnessLength}em"><a href="/FAQ.html#privacy">témoin${witnessNumber ? " n° " + witnessNumber : ""}</a></span>`
   }
 }

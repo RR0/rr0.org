225e7a266b618ba26520dec8dc085d871b970ad0
javarome <javarome@gmail.com>
2024-05-24 14:24:06 +0200

    udb fix

--- udb/index.mjs
+++ udb/index.mjs
@@ -1,8 +1,4 @@
-import { Logger, UdbController, UdbService, WebFileInput, WebReadLine } from '@rr0/udb'
+import { WebUdb } from '@rr0/udb'
 
-const logger = new Logger(false, true)
-const webFileInput = new WebFileInput(logger)
-const webReadLine = new WebReadLine()
-const udbService = new UdbService(logger, webFileInput, webReadLine)
-var win = window
-win.udbController = new UdbController(udbService, logger)
+const webUdb = new WebUdb()
+window.udbController = webUdb.controller
